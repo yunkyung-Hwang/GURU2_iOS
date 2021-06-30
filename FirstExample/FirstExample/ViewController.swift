b325d873a60396eda2a996d57791c1634b946f8d
Hwang <hyk980425@gmail.com>
2021-06-30 13:14:19 +0900

    Day1

---
+++ FirstExample/FirstExample/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  FirstExample
+//
+//  Created by 황윤경 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    /*
+     UI 요소들을 연결하는 방식
+     IBOulet : UI 요소를 변수와 연결하는 것
+     IBAction : UI 요소의 이벤트를 연결하는 것
+     */
+
+    @IBOutlet weak var lable1: UITextField!
+    @IBOutlet weak var label2: UITextField!
+
+    let numberFormatter:NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 2
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("앱 화면이 나타납니다.")
+    }
+    
+    
+    @IBAction func doConvert(_ sender: UIButton) {
+        if let value1 = lable1.text, let number1 = Double(value1){
+            // 섭씨 * 1.8 + 32 = 화씨
+            let fahrenheit = number1 * 1.8 + 32
+            label2.text = numberFormatter.string(from: NSNumber(value: fahrenheit))
+            lable1.resignFirstResponder()
+        }
+        print("버튼이 눌렸습니다.")
+    }
+
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        lable1.resignFirstResponder()
+        label2.resignFirstResponder()
+    }
+    
+}
+
